b102e3874b1547d134a7939f7cbbdb83f156b3fe
Fernando Nicolas Sanchez <sanchez36822171@gmail.com>
2021-10-23 20:33:23 -0300

    Genera GCI para + - id cte :=

---
+++ Sanchez_Fernando_Nicolas/Sintactico.y
@@ -0,0 +1,249 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "y.tab.h"
+#include "funciones.c"
+
+int yystopparser=0;
+FILE *yyin;
+int yyerror(char *mensaje);
+int yylex();
+char *yyltext;
+int conadorDeclaracionesV = 0;
+int conadorDeclaracionesT = 0;
+int numeroPolaca = 0;
+char idAux[30];
+
+
+%}
+%union 
+{ 
+    int intValue; 
+    float floatValue; 
+    char *stringValue; 
+} 
+%start programa
+%token <stringValue> ID
+%token OP_ASIG
+%token CTE
+%token COMEN
+%token DISPLAY
+%token CTE_S
+%token ENTER
+%token OP_SUM
+%token OP_RES
+%token OP_MUL
+%token OP_DIV
+%token PARA
+%token PARC
+%token GET
+%token WHILE
+%token START
+%token END
+%token OR
+%token AND
+%token COMPARADOR
+%token IF
+%token THEN
+%token ELSE
+%token ENDIF
+%token DIM
+%token CORA
+%token CORC
+%token AS
+%token TIPO
+%token COMA
+%token LONG
+%token IN
+%token DO
+%token ENDWHILE
+%token CTE_R
+%token NOT
+%%
+
+programa : programa sentencia {printf("\n---------------------->programa - Start detectado");}
+		 |  sentencia  {printf("\n---------------------->programa - sentencia - Start detectado");} ;
+		
+		
+sentencia : asignacion {printf("\n---------------------->sentencia - asignacion");}
+		  | salida {printf("\n---------------------->sentencia - salida");}
+		  | entrada {printf("\n---------------------->sentencia - entrada");}
+		  | iteracion {printf("\n---------------------->sentencia - iteracion");}
+		  | seleccion {printf("\n---------------------->sentencia - seleccion");}
+		  |	declaracion {printf("\n---------------------->sentencia - declaracion");}
+		  | COMEN {printf("\n");}
+		  | LONG PARA lista PARC {printf("\n---------------------->sentencia - tema especial - long");}
+		  | ciclo_especial {printf("\n---------------------->sentencia - tema especial - cilco especial");}
+		  | ENTER {printf("\n");};
+
+asignacion : ID OP_ASIG expresion {	printf("\n---------------------->asignacion");
+									insertar_en_polaca_operador(":=", numeroPolaca);
+									numeroPolaca++;
+									insertar_en_polaca_id($<stringValue>1, numeroPolaca);
+									numeroPolaca++;
+			
+			};
+		
+salida :    DISPLAY factor {printf("\n---------------------->salida - display");}
+		  | DISPLAY CTE_S {printf("\n---------------------->salida - display");
+					printf("\n---------------------->factor cte STRING");
+			
+				
+				
+						strcpy(idAux,yylval.stringValue);	
+						guardar_cte_string(idAux);
+					
+					
+					
+		 }
+			
+		  		  	  
+		  
+		  ;
+		  
+		
+		  
+		  
+		  
+entrada:    GET ID {printf("\n---------------------->entrada");};
+
+expresion : expresion OP_SUM termino {	printf("\n---------------------->expresion - SUM");
+										insertar_en_polaca_operador("+", numeroPolaca);
+										numeroPolaca++;
+		   }
+		  | expresion OP_RES termino {	printf("\n---------------------->expresion - RES");
+										insertar_en_polaca_operador("-", numeroPolaca);
+										numeroPolaca++;
+		   }
+		  | termino {printf("\n---------------------->expresion - termino");};
+		  
+iteracion: WHILE condicion START programa END {printf("\n---------------------->iteracion - while");};
+
+seleccion :   IF  condicion THEN programa ELSE programa ENDIF {printf("\n---------------------->seleccion - if");}
+			| IF condicion THEN programa ENDIF {printf("\n---------------------->seleccion - if");}
+			;
+
+condicion :   PARA condicion AND comparacion PARC {printf("\n---------------------->condicion");}
+			| PARA condicion OR comparacion PARC {printf("\n---------------------->condicion");}
+			| PARA NOT condicion PARC	{printf("\n---------------------->condicion");}
+			| comparacion 	{printf("\n---------------------->condicion");};
+			
+comparacion: expresion COMPARADOR expresion {printf("\n---------------------->3 - condicion");}
+			|PARA expresion COMPARADOR expresion PARC{printf("\n---------------------->3 - condicion");}
+			;
+
+
+termino   : termino OP_MUL factor {printf("\n---------------------->termino");}
+		  | termino OP_DIV factor {printf("\n---------------------->termino");}
+		  | factor {printf("\n---------------------->termino - factor");};
+
+factor :    ID {printf("\n---------------------->factor - id");
+				insertar_en_polaca_id($<stringValue>1, numeroPolaca);
+				numeroPolaca++;
+
+		}
+		
+		  | CTE {
+				printf("\n---------------------->factor - cte");
+				char* nombre_cte_int = guardar_cte_int(atoi($<stringValue>1));
+				insertar_en_polaca_cte_int(atoi($<stringValue>1), numeroPolaca);
+				numeroPolaca++;
+		}
+		 |CTE_R {
+					printf("\n---------------------->factor cte real");
+					float valor = atof($<stringValue>1);
+					char* nombre_cte_float = guardar_cte_float(valor);
+					
+		 }
+		 	 
+		 
+		 | PARA expresion PARC {printf("\n---------------------->factor - expresion");};
+		 
+declaracion : DIM CORA listav CORC AS CORA listat CORC 
+			{ 
+				guardar_variables_ts();
+				freeArray(&array_nombres_variables);
+				freeArray(&array_tipos_variables);
+				
+				initArray(&array_nombres_variables);
+				initArray(&array_tipos_variables);
+				
+				int controlDeclaracion = conadorDeclaracionesV - conadorDeclaracionesT;
+				conadorDeclaracionesV = 0;
+				conadorDeclaracionesT = 0;
+				if(controlDeclaracion != 0){
+					yyerror("NO COINCIDEN LA CANTIDAD DE PARAMETROS CON LA CANTIDAD DE TIPOS");
+					exit(1);
+				}
+					
+				
+			};
+
+listav : listav COMA ID 
+		{
+			printf("\n---------------------->lista de variables");
+			insertArray(&array_nombres_variables,$<stringValue>3);
+			conadorDeclaracionesV += 1;
+		}
+		| ID 
+		{	
+			printf("\n---------------------->lista de variables - id");
+			insertArray(&array_nombres_variables,$<stringValue>1);
+			conadorDeclaracionesV += 1;
+		}
+		;
+listat : listat COMA TIPO 
+		{
+			printf("\n---------------------->lista tipos");
+			printf("********* tipo %s *********",$<stringValue>3);
+			insertArray(&array_tipos_variables,$<stringValue>3);
+			conadorDeclaracionesT += 1;
+		}
+		| TIPO 
+		{
+			printf("\n---------------------->lista TIPOS - corte");
+			printf("********* tipo %s *********",$<stringValue>1);
+			insertArray(&array_tipos_variables,$<stringValue>1);
+			conadorDeclaracionesT += 1;
+		};
+		
+lista : lista COMA factor {printf("\n---------------------->lista");}
+		| factor {printf("\n---------------------->lista - factor");};
+		
+ciclo_especial : WHILE ID IN CORA lista_expre CORC DO programa ENDWHILE {printf("\n---------------------->ciclo especial");};
+
+lista_expre : lista_expre COMA expresion {printf("\n---------------------->lista de expresiones");}
+			| expresion {printf("\n---------------------->expresion - corte");}
+			;
+
+%%
+
+int main (int argc,char *argv[]){
+
+ if ((yyin=fopen(argv[1],"rt"))==NULL)
+ {
+  	printf("\nNo se puede abrir el archivo: %s\n",argv[1]);
+ }
+ else{
+	initArray(&array_tipos_variables);
+	initArray(&array_nombres_variables);
+    crearTabla();
+	crearPolaca();
+	yyparse();
+	guardar_ts();
+	guardar_gci(numeroPolaca);
+    freeArray(&array_tipos_variables);
+	freeArray(&array_nombres_variables);
+ }
+ fclose(yyin);
+
+ return 0;
+}
+
+int yyerror(char *mensaje)
+	{ 
+ 	  printf("\nSyntax Error: %s\n", mensaje);
+	  system("Pause");
+          exit (1);
+	}
---
+++ Sanchez_Fernando_Nicolas/funciones.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include "array.c"
+
+#define INITIAL_CAPACITY 1
+#define MAX_STRING_LENGTH 30
+#define TABLA_SIMBOLOS "ts.txt"
+#define POLACA "intermedia.txt"
+#define TAM_TABLA 350
+#define TAM_POLACA 350
+#define TAM_NOMBRE 32
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 99
+#define NOT_SUCCESS -99
+
+//funciones tabla simbolos
+void crearTabla();
+void guardar_variables_ts();
+char* guardar_cte_int(int valor);
+void guardar_cte_string(char * valor);
+char* guardar_cte_float(float valor);
+void guardar_ts();
+int existe_simbolo(char * comp);
+int verificar_asignacion(char * valor);
+int existe_between = 0;
+
+//funciones polaca inversa
+void crearPolaca();
+void insertar_en_polaca_cte_int(int cte, int num);
+void insertar_en_polaca_id(char *valor, int num);
+void insertar_en_polaca_operador(char * valor, int num);
+void guardar_gci(int cantidad);
+
+//funciones complementarias
+char* concat(const char *s1, const char *s2);
+
+typedef struct {
+	char nombre[TAM_NOMBRE];
+	char tipo_dato[TAM_NOMBRE];
+	char valor[TAM_NOMBRE];
+	int longitud;
+} simbolo;
+
+simbolo ts[TAM_TABLA];
+simbolo simbolo_busqueda;
+
+typedef struct {
+	char simbolo[TAM_NOMBRE];
+	int numero;
+} polaca;
+
+polaca gci[TAM_POLACA];
+
+FILE * file;
+FILE * filePolaca;
+int between_flag = 0;
+int cant_elem_ts = 0;
+int cantidad_cuerpos;
+int cantidad_bloques = 0;
+char tipo_dato[30];
+char str_aux[30];
+char * ultima_expresion;
+char * ultimo_comparador;
+
+char * ultimo_operador;
+
+int contadorCteString = 0;
+
+void guardar_variables_ts(){
+  int i = 0;
+  for(i; i<array_nombres_variables.used; i++){
+    if(cant_elem_ts<=TAM_TABLA && !existe_simbolo(array_nombres_variables.array[i])){
+      strcpy(ts[cant_elem_ts].nombre,array_nombres_variables.array[i]);
+      ts[cant_elem_ts].longitud = 0;
+      strcpy(ts[cant_elem_ts].tipo_dato,array_tipos_variables.array[i]);
+      strcpy(ts[cant_elem_ts].valor,"-");
+      cant_elem_ts++;
+    }
+    else{
+      if(cant_elem_ts>TAM_TABLA){
+        printf("TABLA DE SIMBOLOS LLENA.\n");
+        exit(1);
+      }
+      else{
+        printf("VARIABLE %s DECLARADA PREVIAMENTE\n", array_nombres_variables.array[i]);
+        exit(1);
+      }
+    }
+  }
+}
+
+void crearTabla(){
+  file = fopen(TABLA_SIMBOLOS, "w");
+  fprintf(file,"%-s\n","NOMBRE                        \tTIPODATO\t\tVALOR\tLONGITUD");
+  fclose(file);
+}
+
+char* guardar_cte_int(int valor) {
+      char  prefijo[] = "_";
+      char constante_string[100];
+      sprintf(constante_string,"%d",valor);
+
+      char* nombre_constante = concat(prefijo, constante_string);
+      if(existe_simbolo(nombre_constante) == FALSE && cant_elem_ts <= TAM_TABLA){
+        strcpy(ts[cant_elem_ts].nombre,nombre_constante);
+        ts[cant_elem_ts].longitud = 0;
+        strcpy(ts[cant_elem_ts].tipo_dato,"CTE_INTEGER");
+        strcpy(ts[cant_elem_ts].valor,constante_string);
+        cant_elem_ts++;
+      }
+      return nombre_constante;
+}
+
+void guardar_cte_string(char * valor) {
+      char nombre_constante[32];
+      /*sprintf(nombre_constante,"_cte_string_%d", contadorCteString);
+      char * returnValue = malloc(sizeof(char)*100);
+      strcpy(returnValue, nombre_constante);
+      if(existe_simbolo(nombre_constante) == FALSE && cant_elem_ts <= TAM_TABLA){
+        strcpy(ts[cant_elem_ts].nombre,valor);
+        ts[cant_elem_ts].longitud = strlen(valor);
+        strcpy(ts[cant_elem_ts].tipo_dato,"CTE_STRING");
+        strcpy(ts[cant_elem_ts].valor,valor);
+        cant_elem_ts++;
+        contadorCteString++;
+      }*/
+	  strcpy(ts[cant_elem_ts].nombre,valor);
+      ts[cant_elem_ts].longitud = strlen(valor);
+      strcpy(ts[cant_elem_ts].tipo_dato,"CTE_STRING");
+      strcpy(ts[cant_elem_ts].valor,"-");
+      cant_elem_ts++;
+      contadorCteString++;
+	  
+
+}
+
+char* guardar_cte_float(float valor) {
+      
+      float constante = valor;
+      char  prefijo[] = "_";
+      char constante_string[100];
+      sprintf(constante_string,"%f",constante);
+      char* nombre_constante = concat(prefijo, constante_string);
+      if(existe_simbolo(nombre_constante) == FALSE && cant_elem_ts <= TAM_TABLA){
+        strcpy(ts[cant_elem_ts].nombre,nombre_constante);
+        ts[cant_elem_ts].longitud = 0;
+        strcpy(ts[cant_elem_ts].tipo_dato,"CTE_FLOAT");
+        strcpy(ts[cant_elem_ts].valor,constante_string);
+        cant_elem_ts++; 
+      }
+      return nombre_constante;
+}
+
+void guardar_ts(){
+  int i = 0;
+  char longitud[3];
+  file = fopen(TABLA_SIMBOLOS,"a");
+  for(i;i<cant_elem_ts;i++){
+    if(ts[i].longitud == 0){
+      strcpy(longitud,"-");
+	   fprintf(file,"%-30s\t%-s\t%20s\t%-30s\n",ts[i].nombre,ts[i].tipo_dato,ts[i].valor,longitud);
+    }
+    else{
+      sprintf(longitud,"%d",ts[i].longitud);
+      //strcpy(longitud,longitud);
+	  //strcpy(longitud,ts[i].longitud);
+	   fprintf(file,"%-30s\t%-s\t%20s\t%-30s\n",ts[i].nombre,ts[i].tipo_dato,ts[i].valor,longitud);
+    }
+
+
+    //%-35s%-20s%-35s%-5s
+  }
+  fclose(file);
+}
+
+int existe_simbolo(char * comp) {
+  char * aux;
+  aux = malloc(sizeof(char)*strlen(comp));
+  strcpy(aux,comp);
+  int i = 0;
+  for( i ; i < cant_elem_ts ; i++ ){
+    if( strcmp(aux, ts[i].nombre) == 0 ){
+      strcpy(simbolo_busqueda.nombre, ts[i].nombre);
+      simbolo_busqueda.longitud = ts[i].longitud;
+      strcpy(simbolo_busqueda.tipo_dato, ts[i].tipo_dato);
+      strcpy(simbolo_busqueda.valor, ts[i].valor);
+      free(aux);
+      return TRUE;
+      }
+  }
+  free(aux);
+  return FALSE;
+}
+
+int verificar_asignacion(char * valor) {
+  if(!existe_simbolo(valor)){
+        return 1;
+  } else {
+        if(strcmp(ultima_expresion, simbolo_busqueda.tipo_dato) == 0 || (strcmp(simbolo_busqueda.tipo_dato, "real") == 0 && strcmp(ultima_expresion, "int") == 0)){ //float
+              return 2;
+        }
+        else {
+              return 3;
+        }
+  }
+}
+
+char* concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+    return result;
+}
+/* Funciones polaca*/
+
+void crearPolaca(){
+  filePolaca = fopen(POLACA, "w");
+  fclose(filePolaca);
+}
+
+
+void insertar_en_polaca_cte_int(int cte, int num){
+	char constante_string[32];
+	sprintf(constante_string,"%d",cte);
+	strcpy(gci[num].simbolo, constante_string);
+	gci[num].numero = num+10;
+}
+
+void insertar_en_polaca_id(char *valor, int num){
+	strcpy(gci[num].simbolo, valor);
+	gci[num].numero = num+10;
+}
+
+
+void insertar_en_polaca_operador(char * valor, int num){
+	strcpy(gci[num].simbolo, valor);
+	gci[num].numero = num+10;
+}
+
+void guardar_gci(int cantidad){
+	
+  filePolaca = fopen(POLACA,"a");
+  int i = 0;
+  
+  for(i;i< cantidad;i++){
+
+	fprintf(filePolaca,"%s\t",gci[i].simbolo);
+  }
+  fprintf(filePolaca,"\n",gci[i].simbolo);
+  i = 0;
+  
+    for(i;i< cantidad;i++){
+
+	fprintf(filePolaca,"%d\t",gci[i].numero);
+  }
+  fclose(filePolaca);
+}
